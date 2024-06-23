f64ab395a793cb1b19e6415c4fea9a94d261d024
umutcanbas <umutbas57@gmail.com>
2024-06-23 17:49:34 +0300

    Favorite slice

--- src/redux/favorite.js
+++ src/redux/favorite.js
@@ -1,30 +1,46 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  favoriteList: [],
+  favoriteEpisodeList: [],
+  favoriteCharacterList: [],
 };
 
-const favorite = createSlice({
+const favoriteSlice = createSlice({
   name: 'favorite',
   initialState,
   reducers: {
-    favoriteEpisode: (state, action) => {
-      const product = action.payload;
-      const isFavorite = state.favoriteList.find(
-        item => item.id === product.id,
-      );
+    addFavoriteEpisode: (state, action) => {
+      const episode = action.payload;
+      const isFavorite = state.favoriteEpisodeList.find(item => item.id === episode.id);
 
-      if (isFavorite) {
-        state.favoriteList = state.favoriteList.filter(
-          item => item.id !== product.id,
-        );
-      } else {
-        state.favoriteList.push(product);
+      if (!isFavorite) {
+        state.favoriteEpisodeList.push(episode);
       }
+    },
+    removeFavoriteEpisode: (state, action) => {
+      const episode = action.payload;
+      state.favoriteEpisodeList = state.favoriteEpisodeList.filter(item => item.id !== episode.id);
+    },
+    addFavoriteCharacter: (state, action) => {
+      const character = action.payload;
+      const isFavorite = state.favoriteCharacterList.find(item => item.id === character.id);
+
+      if (!isFavorite) {
+        state.favoriteCharacterList.push(character);
+      }
+    },
+    removeFavoriteCharacter: (state, action) => {
+      const character = action.payload;
+      state.favoriteCharacterList = state.favoriteCharacterList.filter(item => item.id !== character.id);
     },
   },
 });
 
-export const {favoriteEpisode} = favorite.actions;
+export const {
+  addFavoriteEpisode,
+  removeFavoriteEpisode,
+  addFavoriteCharacter,
+  removeFavoriteCharacter,
+} = favoriteSlice.actions;
 
-export default favorite.reducer;
+export default favoriteSlice.reducer;
