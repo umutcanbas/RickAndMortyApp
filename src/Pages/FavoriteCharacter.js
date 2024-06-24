2ef50f861c77fe1bc5ef193cf1727ac2dde94f01
umutcanbas <umutbas57@gmail.com>
2024-06-24 15:06:17 +0300

    Remove favorite function added

--- src/Pages/FavoriteCharacter.js
+++ src/Pages/FavoriteCharacter.js
@@ -6,45 +6,74 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
-import {useSelector} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { addFavoriteCharacter, removeFavoriteCharacter } from '../redux/favorite';
 
 import routes from '../navigation/routes';
 
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+
+import HeartIcon from '../assets/svg/heart.svg';
 
 const FavoriteCharacter = () => {
   const favoriteCharacters = useSelector(
     state => state.favorite.favoriteCharacterList,
   );
+  const dispatch = useDispatch();
 
   const navigation = useNavigation();
+
+  const handlePress = (character) => {
+    const isFavorite = favoriteCharacters.some(c => c.id === character.id);
+
+    if (isFavorite) {
+      dispatch(removeFavoriteCharacter(character));
+    } else {
+      dispatch(addFavoriteCharacter(character));
+    }
+  };
 
   return (
     <View style={styles.container}>
       <FlatList
         data={favoriteCharacters}
-        renderItem={({item}) => (
-          <TouchableOpacity
-            activeOpacity={0.8}
-            onPress={() =>
-              navigation.navigate(routes.CHARACTER_DETAIL, {character: item})
-            }
-            style={styles.content}>
-            <View style={styles.textContainer}>
-              <Text style={styles.text}>{item.name}</Text>
-            </View>
+        renderItem={({ item }) => {
+          const isFavorite = favoriteCharacters.some(c => c.id === item.id);
 
-            <View style={styles.imageContainer}>
-              <Image
-                source={{uri: item.image}}
-                style={styles.image}
-                resizeMode="cover"
-              />
-            </View>
-          </TouchableOpacity>
-        )}
+          return (
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={() =>
+                navigation.navigate(routes.CHARACTER_DETAIL, { character: item })
+              }
+              style={styles.content}>
+              <View style={styles.textContainer}>
+                <Text style={styles.text}>{item.name}</Text>
+              </View>
+
+              <View style={styles.imageContainer}>
+                <Image
+                  source={{ uri: item.image }}
+                  style={styles.image}
+                  resizeMode="cover"
+                />
+              </View>
+
+              <TouchableOpacity
+                style={styles.favoriteContainer}
+                onPress={() => handlePress(item)}>
+                <HeartIcon
+                  width={30}
+                  height={30}
+                  fill={isFavorite ? '#dd0000' : '#ffffff'}
+                />
+              </TouchableOpacity>
+            </TouchableOpacity>
+          );
+        }}
         keyExtractor={item => item.id.toString()}
       />
     </View>
@@ -80,5 +109,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 21,
     color: 'black',
+  },
+  favoriteContainer: {
+    position: 'absolute',
+    right: 25,
+    top: 50,
   },
 });
